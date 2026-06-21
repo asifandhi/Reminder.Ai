a7f55c5a0739390538364330beeb023b02ef177a
asifandhi <asifandhi267@gmail.com>
2026-06-21 08:43:15 +0530

    version 3.1.1.2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ import session from 'express-session';
 const app = express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN || process.env.CORS_ORIGIN2,
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }));
 app.use(express.json({limit:"20kb"}))
