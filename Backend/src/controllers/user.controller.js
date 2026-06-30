a510a34fafc6f49765f77cc556058d8a677d3494
asifandhi <asifandhi267@gmail.com>
2026-06-30 16:54:08 +0530

    version 3.2.0.2.1

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import apiError from "../utils/apiError.js";
-import apiResponse from "../utils/ApiResponse.js";
+import apiResponse from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
